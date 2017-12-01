b7f6782035f9924c5b8a1b5aa71b627e7d54399f
jiat6 <jiat6@chinaunicom.cn>
2017-12-01 11:29:17 +0800

    Initial commit

---
+++ spring-cloud-client-config/src/main/java/com/example/springcloudclientconfig/SpringCloudClientConfigApplication.java
@@ -0,0 +1,12 @@
+package com.example.springcloudclientconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudClientConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudClientConfigApplication.class, args);
+	}
+}
---
+++ spring-cloud-server-config/src/main/java/com/example/springcloudserverconfig/SpringCloudServerConfigApplication.java
@@ -0,0 +1,14 @@
+package com.example.springcloudserverconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class SpringCloudServerConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudServerConfigApplication.class, args);
+	}
+}
